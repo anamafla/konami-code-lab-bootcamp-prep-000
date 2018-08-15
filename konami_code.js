a0fa89a1f0a89f8a25bbedc206a2c9d21abb7a17
Ana Mafla <anamafla@hotmail.com>
2018-08-15 15:04:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,4 +15,16 @@ function init() {
   // your code here
   console.log('I am in function init')
   document.body.addEventListener('keydown', alert('You click'));
+  
+  function onKeyDownHandler(e) {
+    const key = e.key;
+    
+    if ( key === codes[index]) {
+      index++;
+      
+      if(index === alphabet.length) 
+      { alert("Hurray"); 
+      index = 0;
+    }
+  }
 }
